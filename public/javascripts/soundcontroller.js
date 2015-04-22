8645ff3da144bac8037f6d95b0bcad8a2c4fb25b
David Nääs <david_naas@hotmail.com>
2015-04-22 16:20:49 +0200

    fancy transitions and cross fade

--- public/javascripts/soundcontroller.js
+++ public/javascripts/soundcontroller.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
+
+	frames = $('.space-frame');
+
+	for(i = 0; i < frames.length; i++ ){
+		opacity = $(frames[i]).css('opacity', 0);
+		audio = $(frames[i]).find(".audioplayer");
+		audio[0].volume = 0;
+	}
+
+
 	$(window).scroll(function () {
 		frames = $('.space-frame');
 
 		for(i = 0; i < frames.length; i++ ){
 
 			opacity = $(frames[i]).css('opacity');
-			//console.log(opacity);
+			console.log(opacity);
 			audio = $(frames[i]).find(".audioplayer");
 			audio[0].volume = opacity;
 		}
